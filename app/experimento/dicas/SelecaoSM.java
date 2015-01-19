6116ba4cef09d22a7ed2ad2aeb1474f3af567afb
Priscylla Silva <mss.priscylla@gmail.com>
2015-01-19 13:45:23 -0300

    experimento v0.2

--- app/experimento/dicas/SelecaoSM.java
+++ app/experimento/dicas/SelecaoSM.java
@@ -3,6 +3,7 @@
  */
 package experimento.dicas;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -42,6 +43,8 @@ public class SelecaoSM implements TipoSelecao {
 		ServiceLog log = new ServiceLog();
 		StudentModel modelo = new StudentModel();
 		
+		List<Logexp> novaLista = new ArrayList<Logexp>();
+		
 		TypedQuery<Logexp> query = JPA.em().createQuery("SELECT l FROM Logexp l WHERE l.idStudent = :idStudent", Logexp.class);
 		List<Logexp> list = query.setParameter("idStudent", student.getId()).getResultList();
 		
@@ -52,35 +55,32 @@ public class SelecaoSM implements TipoSelecao {
 		
 		for (Logexp logexp : list) {
 			
-			if(logexp.getIdQuestion().intValue() != question.getId().intValue()){ // senão for a questão corrente
-				list.remove(logexp);
-			}
-			if(logexp.getIdDica()==null && logexp.getIdDica()==""){
-				list.remove(logexp);
+			if(logexp.getIdQuestion().intValue()==question.getId().intValue()){
+				novaLista.add(logexp);
 			}
 		}
 		
-		if(list.size()==1){
+		if(novaLista.size()==1){
 			log.save(question, student, null, false, "3", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica3();
 		}
-		if(list.size()==2){
+		if(novaLista.size()==2){
 			log.save(question, student, null, false, "4", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica4();
 		}
-		if(list.size()==3){
+		if(novaLista.size()==3){
 			log.save(question, student, null, false, "5", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica5();						
 		}
-		if(list.size()==4){
+		if(novaLista.size()==4){
 			log.save(question, student, null, false, "2", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica2();						
 		}
-		if(list.size()==5){
+		if(novaLista.size()==5){
 			log.save(question, student, null, false, "1", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica1();
 		}
-		if(list.size()>=6){
+		if(novaLista.size()>=6){
 			log.save(question, student, null, false, "6", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica5();
 		}
@@ -94,6 +94,8 @@ public class SelecaoSM implements TipoSelecao {
 		ServiceLog log = new ServiceLog();
 		StudentModel modelo = new StudentModel();
 		
+		List<Logexp> novaLista = new ArrayList<Logexp>();
+		
 		TypedQuery<Logexp> query = JPA.em().createQuery("SELECT l FROM Logexp l WHERE l.idStudent = :idStudent", Logexp.class);
 		List<Logexp> list = query.setParameter("idStudent", student.getId()).getResultList();
 		
@@ -104,35 +106,32 @@ public class SelecaoSM implements TipoSelecao {
 		
 		for (Logexp logexp : list) {
 			
-			if(logexp.getIdQuestion().intValue() != question.getId().intValue()){ // senão for a questão corrente
-				list.remove(logexp);
-			}
-			if(logexp.getIdDica()==null && logexp.getIdDica()==""){
-				list.remove(logexp);
+			if(logexp.getIdQuestion().intValue()==question.getId().intValue()){
+				novaLista.add(logexp);
 			}
 		}
 		
-		if(list.size()==1){
+		if(novaLista.size()==1){
 			log.save(question, student, null, false, "2", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica2();
 		}
-		if(list.size()==2){
+		if(novaLista.size()==2){
 			log.save(question, student, null, false, "3", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica3();
 		}
-		if(list.size()==3){
+		if(novaLista.size()==3){
 			log.save(question, student, null, false, "4", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica4();						
 		}
-		if(list.size()==4){
+		if(novaLista.size()==4){
 			log.save(question, student, null, false, "1", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica1();						
 		}
-		if(list.size()==5){
+		if(novaLista.size()==5){
 			log.save(question, student, null, false, "5", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica5();
 		}
-		if(list.size()>=6){
+		if(novaLista.size()>=6){
 			log.save(question, student, null, false, "6", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica5();
 		}
--- app/experimento/dicas/SelecaoTopDown.java
+++ app/experimento/dicas/SelecaoTopDown.java
@@ -3,6 +3,7 @@
  */
 package experimento.dicas;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -24,46 +25,47 @@ public class SelecaoTopDown implements TipoSelecao {
 	public String getDica(Question question, Student student) {
 		ServiceLog log = new ServiceLog();
 		StudentModel modelo = new StudentModel();
+		List<Logexp> novaLista = new ArrayList<Logexp>();
 		
 		TypedQuery<Logexp> query = JPA.em().createQuery("SELECT l FROM Logexp l WHERE l.idStudent = :idStudent", Logexp.class);
 		List<Logexp> list = query.setParameter("idStudent", student.getId()).getResultList();
 		
 		if(list.isEmpty()){
+			System.out.println("Lista vazia");
 			log.save(question, student, null, false, "1", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica1();
 		}
+		
+		System.out.println("Tamanho da lista: "+ list.size());
 		
 		for (Logexp logexp : list) {
 			
-			if(logexp.getIdQuestion().intValue() != question.getId().intValue()){ // senão for a questão corrente
-				list.remove(logexp);
-			}
-			if(logexp.getIdDica()==null && logexp.getIdDica()==""){
-				list.remove(logexp);
+			if(logexp.getIdQuestion().intValue()==question.getId().intValue()){
+				novaLista.add(logexp);
 			}
 		}
 		
-		if(list.size()==1){
+		if(novaLista.size()==1){
 			log.save(question, student, null, false, "1", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica1();
 		}
-		if(list.size()==2){
+		if(novaLista.size()==2){
 			log.save(question, student, null, false, "2", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica2();
 		}
-		if(list.size()==3){
+		if(novaLista.size()==3){
 			log.save(question, student, null, false, "3", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica3();						
 		}
-		if(list.size()==4){
+		if(novaLista.size()==4){
 			log.save(question, student, null, false, "4", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica4();						
 		}
-		if(list.size()==5){
+		if(novaLista.size()==5){
 			log.save(question, student, null, false, "5", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica5();
 		}
-		if(list.size()>=6){
+		if(novaLista.size()>=6){
 			log.save(question, student, null, false, "6", Calendar.getInstance(), ""+modelo.getValue(student), false);
 			return question.getDica5();
 		}
