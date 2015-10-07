37ec36df02fef70a8a05a170336a27d0f761a417
Priscylla Silva <mss.priscylla@gmail.com>
2015-10-07 11:21:31 -0300

    Add botão submit na resolução de questões de texto

--- app/controllers/resolution/ResolutionController.java
+++ app/controllers/resolution/ResolutionController.java
@@ -8,8 +8,10 @@ import pedagogical.Planning;
 import pedagogical.impl.PlanningImpl;
 import play.data.DynamicForm;
 import play.data.Form;
+import play.data.DynamicForm.Dynamic;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.Http.Request;
 import repositories.questions.impl.QuestionRepositoryImpl;
 
 /**
@@ -26,7 +28,12 @@ public class ResolutionController extends Controller {
 		return ok(views.html.resolution.index.render(question, form));
 	}
 	
-	public static Result submit(){
+	public static Result submit(Long id){
+		
+		Form<Dynamic> requestForm = form.bindFromRequest();
+    	String answer = requestForm.data().get("answer");
+    	
+    	
 		return TODO;
 	}
 
