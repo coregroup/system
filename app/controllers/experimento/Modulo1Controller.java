6b194af4c082a2f4d1fcb1379f3a676cfbb5a94b
Priscylla Silva <mss.priscylla@gmail.com>
2017-01-08 14:32:13 -0300

    revert experimento 0.4 e 0.3

--- app/controllers/experimento/Modulo1Controller.java
+++ app/controllers/experimento/Modulo1Controller.java
@@ -8,7 +8,7 @@ import java.util.List;
 import models.curriculum.Question;
 import models.curriculum.Solution;
 import models.curriculum.Topic;
-import models.users.Student;
+import models.users.User;
 import play.data.DynamicForm;
 import play.data.Form;
 import play.mvc.Controller;
@@ -18,8 +18,6 @@ import services.topics.TopicService;
 import services.topics.impl.TopicServiceImpl;
 import services.users.UserService;
 import services.users.impl.UserServiceImpl;
-
-import com.avaje.ebean.Ebean;
 
 public class Modulo1Controller extends Controller{
 	
@@ -33,27 +31,26 @@ public class Modulo1Controller extends Controller{
 		Question question = planning.nextQuestion(topic);
 		
 		///////////////////////
-		String email = session().get("email");
-    	Student student = (Student) userService.findByEmail(email);
-		Long idLastQuestion = null;
-		
-		List<Solution> listSolution = Ebean.find(Solution.class).where().eq("user_id", student.getId().toString()).findList();
-		
-		for (int i = 0; i < listSolution.size(); i++) {
-			if(topic.id.equals(listSolution.get(i).getQuestion().getTopics().get(0).id)){
-				idLastQuestion = listSolution.get(i).getQuestion().id;
-			}
-		}
-		if(idLastQuestion==null){
-			return ok(views.html.resolution.index.render(question, form));
-		}
-			question = planning.nextQuestion(topic, idLastQuestion);
+//		String email = session().get("email");
+//    	User student = userService.findByEmail(email);
+//		List<Solution> solutions = student.getSolutions();
+//		Long idLastQuestion = null;
+//		for (int i = 0; i < solutions.size(); i++) {
+//			if(topic.id.equals(solutions.get(i).getQuestion().getTopics().get(0).id)){
+//				idLastQuestion = solutions.get(i).getQuestion().id;
+//			}
+//		}
+//		
+//		if(idLastQuestion==null){
+//			System.out.println("\n********************************************\n");
+//			System.out.println("já tinha algumas resolvidas");
+//			System.out.println("\n********************************************\n");
+//			return ok(views.html.resolution.index.render(question, form));
+//		}
+//			question = planning.nextQuestion(topic, idLastQuestion);
+//		
 		///////////////////
 		
-			if(question == null){
-				flash("success", "Você finalizou todas as questões do módulo!");
-				return redirect(controllers.pedagogical.routes.PlanningController.index());
-			}
 		
 		return ok(views.html.resolution.index.render(question, form));
 	}
--- app/its/domain/impl/EvaluatorImpl.java
+++ app/its/domain/impl/EvaluatorImpl.java
@@ -3,14 +3,7 @@
  */
 package its.domain.impl;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Scanner;
-
 import models.CorrectionType;
-import models.QuestionType;
 import models.curriculum.Question;
 import models.curriculum.Solution;
 import its.domain.Evaluator;
@@ -29,10 +22,6 @@ public class EvaluatorImpl implements Evaluator {
 		Question question = solution.getQuestion();
 		String studentAnswer = solution.getAnswer();
 		
-		if(question.getQuestionType().equals(QuestionType.UPLOAD)){
-			return evaluateUploadQuestion(solution);
-		}
-		
 		if(question.getCorrectionType().equals(CorrectionType.AUTOMATIC)){
 			String correctAnswer = question.getAnswer();
 			if(correctAnswer.equals(studentAnswer))
@@ -41,38 +30,6 @@ public class EvaluatorImpl implements Evaluator {
 				return false;
 		}
 		
-		return false;
-	}
-	
-	private boolean evaluateUploadQuestion(Solution solution){
-		/**
-		 * Repurar arquivo com casos de testes e extrair INPUT e OUTPUT
-		 */
-		Question question = solution.getQuestion();
-		File arquivo = new File("public/output-codes/" + question.getAnswer());
-		try( InputStream in = new FileInputStream(arquivo) ){
-		  Scanner scan = new Scanner(in);
-		  String output = "";
-		  String input = "";
-		  boolean startOutput = false;
-		  
-		  while( scan.hasNext() ){
-			  String line = scan.nextLine();
-			  if(line.equals("OUTPUT")){
-				  startOutput = true;
-			  }
-			  if(startOutput)
-				  output =  output + line + "\n";
-			  else
-				  input = input + line + "\n"; 
-		  }
-		  input = input.replaceAll("INPUT", "");
-		  output = output.replaceAll("OUTPUT", "");
-//		  System.out.println("Entrada: " + input);
-//		  System.out.println("Saida: " + output);
-		}catch(IOException ex){
-		  ex.printStackTrace();
-		}
 		return false;
 	}
 
