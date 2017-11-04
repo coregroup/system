58ed9725f3f4203bdb9dde88806fcfb9f78b9a2d
Priscylla Silva <mss.priscylla@gmail.com>
2017-11-03 21:56:25 -0300

    Professor pode avaliar as soluções manualmente

--- app/controllers/resolution/TestCorrectionController.java
+++ app/controllers/resolution/TestCorrectionController.java
@@ -3,6 +3,7 @@
  */
 package controllers.resolution;
 
+import models.curriculum.Solution;
 import play.mvc.Controller;
 import play.mvc.Result;
 import services.solutions.SolutionService;
@@ -26,7 +27,27 @@ public class TestCorrectionController extends Controller{
 	}
 	
 	public static Result details(Long id){
-		return TODO;
+		SolutionService service = new SolutionServiceImpl();
+		Solution solution = service.findById(id);
+		return ok(views.html.correction.uncorrected.details.render(solution));
+	}
+	
+	public static Result submitCorrect(Long id){
+		SolutionService service = new SolutionServiceImpl();
+		Solution solution = service.findById(id);
+		solution.setEvaluation(1);
+		service.update(solution);
+		flash("info", "Avaliação da solução realizada!");
+		return redirect(controllers.resolution.routes.TestCorrectionController.listUncorrectSolutions(0, "id", "asc", ""));
+	}
+	
+	public static Result submitIncorrect(Long id){
+		SolutionService service = new SolutionServiceImpl();
+		Solution solution = service.findById(id);
+		solution.setEvaluation(0);
+		service.update(solution);
+		flash("info", "Avaliação da solução realizada!");
+		return redirect(controllers.resolution.routes.TestCorrectionController.listUncorrectSolutions(0, "id", "asc", ""));
 	}
 
 }
