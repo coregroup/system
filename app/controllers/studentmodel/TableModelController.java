d99dc4d6cfb0b845e7c340050e0f453746bd90ff
Igor Santana <igoricm1@gmail.com>
2018-05-05 16:08:30 -0300

    Criação da TableModelController, da página table.scala.html e adição de rotas

---
+++ app/controllers/studentmodel/TableModelController.java
@@ -0,0 +1,19 @@
+package controllers.studentmodel;
+
+import controllers.authentication.UserAuthenticatedSecured;
+import models.its.StudentModel;
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.mvc.Security;
+import services.its.studentmodel.StudentModelService;
+import services.its.studentmodel.impl.StudentModelServiceImpl;
+
+@Security.Authenticated(UserAuthenticatedSecured.class)
+public class TableModelController extends Controller{
+	
+	public static Result index() {
+		StudentModelService service = new StudentModelServiceImpl();
+		StudentModel studentModel = service.findById(new Long(1));
+		return ok(views.html.studentmodel.table.render(studentModel));
+	}
+}
