f2b95b6a08918180b69baf4076da1d05e059e84f
Igor Santana <igoricm1@gmail.com>
2018-06-06 16:45:24 -0300

    Alterações no Vertical Bar

--- app/controllers/studentmodel/VerticalBarModelController.java
+++ app/controllers/studentmodel/VerticalBarModelController.java
@@ -1,5 +1,8 @@
 package controllers.studentmodel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import controllers.authentication.UserAuthenticatedSecured;
 import models.its.StudentModel;
 import play.mvc.Controller;
@@ -10,11 +13,17 @@ import services.its.studentmodel.impl.StudentModelServiceImpl;
 
 @Security.Authenticated(UserAuthenticatedSecured.class)
 public class VerticalBarModelController extends Controller{
-
+	
 	public static Result index() {
+		List<Double> valores = new ArrayList<>();
 		StudentModelService service = new StudentModelServiceImpl();
 		StudentModel studentModel = service.findById(new Long(1));
-		return ok(views.html.studentmodel.verticalBar.render(studentModel));
+		
+		for(int i = 0; i < studentModel.studentModelUnits.size(); i++) {
+			valores.add(studentModel.studentModelUnits.get(i).getMastered() * 100);
+		}
+		
+		return ok(views.html.studentmodel.verticalBar.render(studentModel, valores));
 	}
 	
 }
