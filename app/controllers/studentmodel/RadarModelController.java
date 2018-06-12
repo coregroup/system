cde1b216665d40d1df24127d29dd1cbf79c741f3
raniel <ranielcorreia1997@gmail.com>
2018-06-12 11:27:26 -0300

    modificação na classe controller HorizontalBarModelController, modficação do arquivo horizontalBar.scala.html, adição de rotas no arquivo routes e criação e implementação da classe controller RadarModelController e do arquivo radar.scala.html

--- app/controllers/studentmodel/RadarModelController.java
+++ app/controllers/studentmodel/RadarModelController.java
@@ -3,29 +3,26 @@ package controllers.studentmodel;
 import java.util.ArrayList;
 import java.util.List;
 
-import controllers.authentication.UserAuthenticatedSecured;
 import models.its.StudentModel;
 import play.mvc.Controller;
 import play.mvc.Result;
-import play.mvc.Security;
 import services.its.studentmodel.StudentModelService;
 import services.its.studentmodel.impl.StudentModelServiceImpl;
 
-@Security.Authenticated(UserAuthenticatedSecured.class)
-public class HorizontalBarModelController extends Controller {
-
+public class RadarModelController extends Controller {
+	
 	public static Result index() {
-		List<Double> value = new ArrayList<>();
+		List<Double> valores = new ArrayList<>();
+
 		StudentModelService service = new StudentModelServiceImpl();
 		StudentModel studentModel =  service.findById(new Long(1));
 		
 		for(int i = 0; i < studentModel.studentModelUnits.size(); i++) {
-			value.add(studentModel.studentModelUnits.get(i).getMastered() * 100);
+			valores.add(studentModel.studentModelUnits.get(i).getMastered() * 100);
 		}
-		
-		return ok(views.html.studentmodel.horizontalBar.render(studentModel, value));
+		return ok(views.html.studentmodel.radar.render(studentModel,valores));
 
 		
 	}
-		
+
 }
